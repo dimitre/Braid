d3c88df95ffb6db335dde06923c2d9ab7c3d205a
Dimitre <dimitre@users.noreply.github.com>
2026-06-30 10:56:09 -0300

    bootstrap

---
+++ braid.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+set -e
+
+git pull
+
+# Run libs.sh and capture CHALET_PATH output
+CHALET_PATH=$(./libs/libs.sh | grep "^CHALET_PATH=" | cut -d= -f2 || true)
+
+# Add chalet to PATH if found
+if [[ -n "$CHALET_PATH" ]]; then
+    export PATH="$CHALET_PATH:$PATH"
+fi
+
+chalet buildrun feed
+# cd ofGen
+# ./compile.sh
+
+# cd ../examples/demos/organicText
+
+# if command -v ofgen >/dev/null 2>&1; then
+#     ofgen templates=chalet buildrun
+# else
+#     ../../../ofgen/dist/ofgen templates=chalet buildrun
+# fi
+
+# cd ../../..
